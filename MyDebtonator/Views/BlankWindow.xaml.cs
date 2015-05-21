b7b07e14f5457965a73613c9b2de795cb39bd968
Joseph King <Joseph_Anthony_King@yahoo.com>
2015-05-20 19:50:36 -0700

    Initial Commit

---
+++ MyDebtonator/Views/BlankWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyDebtonator.Views
+{
+    /// <summary>
+    /// Interaction logic for BlankWindow.xaml
+    /// </summary>
+    public partial class BlankWindow : Window
+    {
+        public BlankWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
