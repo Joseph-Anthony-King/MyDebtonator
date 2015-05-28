b9ad99fdb1eb69c5c9c32f1eed8723db23ee8f83
Joseph King <Joseph_Anthony_King@yahoo.com>
2015-05-27 21:51:07 -0700

    Add the Serializer to Helpers

---
+++ MyDebtonator/Helpers/Serializer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using MyDebtonator.Models;
+
+namespace MyDebtonator.Helpers
+{
+    public class Serializer
+    {
+        #region Constructor
+
+        public Serializer() { }
+
+        #endregion
+
+        #region Methods
+
+        public void SerializeRepository(string fileName, UserRepository userRepository)
+        {
+            using (Stream stream = File.Open(fileName, FileMode.OpenOrCreate))
+            {
+                using (DeflateStream compressedStream = new DeflateStream(stream, CompressionMode.Compress))
+                {
+                    BinaryFormatter bFormatter = new BinaryFormatter();
+                    bFormatter.Serialize(compressedStream, userRepository);
+                }
+            }
+        }
+
+        public UserRepository DeserializeRepository(string fileName)
+        {
+            UserRepository userRepository = new UserRepository();
+
+            using (Stream stream = File.Open(fileName, FileMode.OpenOrCreate))
+            {
+                using (DeflateStream decompressStream = new DeflateStream(stream, CompressionMode.Decompress))
+                {
+                    BinaryFormatter bFormatter = new BinaryFormatter();
+
+                    if (stream.Length > 0)
+                    {
+                        userRepository = (UserRepository)bFormatter.Deserialize(decompressStream);
+                    }
+                }
+            }
+
+            return userRepository;
+        }
+
+        #endregion
+    }
+}
