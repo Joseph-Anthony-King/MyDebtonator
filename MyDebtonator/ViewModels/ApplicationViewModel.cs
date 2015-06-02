57bb687a7b6755cf1d8c49f0740c1a7941d43586
Joseph King <Joseph_Anthony_King@yahoo.com>
2015-06-01 21:50:03 -0700

    Added Exit Application Command to ApplicationViewModel

---
+++ MyDebtonator/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using MyDebtonator.Helpers;
+
+namespace MyDebtonator.ViewModels
+{
+    public class ApplicationViewModel : ObservableObject
+    {
+        private ICommand _exitApplicationCommand;
+
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                return _exitApplicationCommand;
+            }
+            set
+            {
+                _exitApplicationCommand = value;
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            ExitApplicationCommand = new RelayCommand(ExitApplication);
+        }
+
+        public void ExitApplication(object obj)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
