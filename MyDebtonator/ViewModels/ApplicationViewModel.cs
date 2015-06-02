eaeeff71a678a57e8cc404b3124845dcd2f89888
Joseph King <Joseph_Anthony_King@yahoo.com>
2015-06-01 22:13:32 -0700

    Add OpenCalculatorCommand & AboutMessageCommand

--- MyDebtonator/ViewModels/ApplicationViewModel.cs
+++ MyDebtonator/ViewModels/ApplicationViewModel.cs
@@ -10,7 +10,39 @@ namespace MyDebtonator.ViewModels
 {
     public class ApplicationViewModel : ObservableObject
     {
+        #region Fields
+
+        private ICommand _openCalculatorCommand;
+        private ICommand _aboutMessageCommand;
         private ICommand _exitApplicationCommand;
+
+        #endregion
+
+        #region Properties
+
+        public ICommand OpenCalculatorCommand
+        {
+            get
+            {
+                return _openCalculatorCommand;
+            }
+            set
+            {
+                _openCalculatorCommand = value;
+            }
+        }
+
+        public ICommand AboutMessageCommand
+        {
+            get
+            {
+                return _aboutMessageCommand;
+            }
+            set
+            {
+                _aboutMessageCommand = value;
+            }
+        }
 
         public ICommand ExitApplicationCommand
         {
@@ -24,14 +56,38 @@ namespace MyDebtonator.ViewModels
             }
         }
 
+        #endregion
+
+        #region Constructor
+
         public ApplicationViewModel()
         {
+            OpenCalculatorCommand = new RelayCommand(OpenCalculator);
+            AboutMessageCommand = new RelayCommand(AboutMessage);
             ExitApplicationCommand = new RelayCommand(ExitApplication);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void OpenCalculator(object obj)
+        {
+            System.Diagnostics.Process.Start("Calc");
+        }
+
+        public void AboutMessage(object obj)
+        {
+            string message = "My Debtonator \n\nProgrammed by Joseph King\n\nTitle by Carla Olivares\n\n© 2015";
+
+            MessageBox.Show(message, "About My Debtonator", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         public void ExitApplication(object obj)
         {
             Application.Current.Shutdown();
         }
+
+        #endregion
     }
 }
